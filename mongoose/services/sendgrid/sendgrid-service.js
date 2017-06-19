36a65f987c6c347274b15f1c62000bb9cd0c1d3f
Thang Le Quoc <silverhair.guy@gmail.com>
2017-06-19 16:10:42 +0700

    manipulate list

--- mongoose/services/sendgrid/sendgrid-service.js
+++ mongoose/services/sendgrid/sendgrid-service.js
@@ -6,7 +6,8 @@ let sg = require('sendgrid')(apikey);
 
 const chalk = require('chalk');
 
-let recipientRequestPath = config.get('sendgrid.requestPath.recipients');
+let recipientsRequestPath = config.get('sendgrid.requestPath.recipients');
+let listsRequestPath = config.get('sendgrid.requestPath.lists');
 
 
 let self = module.exports = {
@@ -17,7 +18,7 @@ let self = module.exports = {
   addRecipient: function (user) {
     let deferred = Q.defer();
     let request = sg.emptyRequest();
-    request.path = recipientRequestPath;
+    request.path = recipientsRequestPath;
     request.body = [{
       "email": user.email,
       "first_name": user.user_metadata.first_name,
@@ -25,6 +26,27 @@ let self = module.exports = {
     }];
     request.method = 'POST';
 
+    sg.API(request, (err, response) => {
+      if (err) deferred.reject(err);
+      deferred.resolve(response.body);
+    });
+    return deferred.promise;
+  },
+
+  /**
+   * Update recipient
+   */
+  updateRecipient: function (user) {
+    let deferred = Q.defer();
+
+    let request = sg.emptyRequest();
+    request.path = recipientsRequestPath;
+    request.body = [{
+      "email": user.email,
+      "first_name": user.user_metadata.first_name,
+      "last_name": user.user_metadata.last_name
+    }];
+    request.method = 'PATCH';
     sg.API(request, (err, response) => {
       if (err) deferred.reject(err);
       deferred.resolve(response.body);
@@ -41,14 +63,53 @@ let self = module.exports = {
     //request.queryParams["page"] = '1';
     //request.queryParams["page_size"] = '1';
     request.method = 'GET';
-    request.path = recipientRequestPath;
+    request.path = recipientsRequestPath;
 
     sg.API(request, (err, response) => {
       if (err) deferred.reject(err);
       deferred.resolve(response.body);
     });
     return deferred.promise;
+  },
+
+  createList: function (listname) {
+    let deferred = Q.defer();
+    let request = sg.emptyRequest();
+    request.body = {
+      "name": listname
+    };
+
+    request.method = 'POST';
+    request.path = listsRequestPath;
+    sg.API(request, (err, response) => {
+      if (err) deferred.reject(err);
+      deferred.resolve(response.body);
+    });
+    return deferred.promise;
+  },
+
+  getLists: function () {
+    let deferred = Q.defer();
+    let request = sg.emptyRequest();
+    request.method = 'GET';
+    request.path = listsRequestPath;
+    sg.API(request, (err, response) => {
+      if (err) deferred.reject(err);
+      deferred.resolve(response.body);
+    });
+    return deferred.promise;
+  },
+
+  removeList: function (listId) {
+    let deferred = Q.defer();
+    let request = sg.emptyRequest();
+    request.queryParams["delete_contacts"] = 'false';
+    request.method = 'DELETE';
+    request.path = listsRequestPath + '/' + listId;
+    sg.API(request, (error, response) => {
+      if (err) deferred.reject(err);
+      deferred.resolve(response.body);
+    })
+    return deferred.promise;
   }
-
-
 }
